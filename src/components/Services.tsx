4cd995f09e345052322c846fddc21e58f950d9be
MrTussa <dd993736@gmail.com>
2024-09-30 22:14:58 +0500

    Added: our services component

---
+++ src/components/Services.tsx
@@ -0,0 +1,52 @@
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[3rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+
+// TODO: заменить на картинки
+
+const Services = () => {
+  return (
+    <section className="bg-black-secondary w-full px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 ">
+      <h2 className="text-7xl mb-4">Наши услуги</h2>
+      <BentoGrid className=" mx-auto">
+        {items.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            className={item.className}
+            id={i}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+const items = [
+  {
+    title: "Сайты",
+    description:
+      "Веб сайты под ключ, Лендинги, интернет магазины, доработки сайтов, корпоративный сайт - все это можно заказать у нас!",
+    header: <Skeleton />,
+    className: "md:row-span-2 md:flex-col sm:flex-row-reverse",
+  },
+  {
+    title: "Телеграм Боты",
+    description:
+      "Сделаем ботов анонимных сообщений, боты интернет магазинов с поддержкой различных языков",
+    header: <Skeleton />,
+    className: " flex-row-reverse",
+  },
+  {
+    title: "3Д Модели и Архитектура",
+    description: "3Д модели любой сложности, Архитектура, Чертежи, Анимации",
+    header: <Skeleton />,
+    className: " flex-row-reverse",
+  },
+];
+
+export default Services;
