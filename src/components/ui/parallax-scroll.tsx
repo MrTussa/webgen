2c6b3d352b3ebbe31a3890d177b36c6b40d4c5cc
MrTussa <dd993736@gmail.com>
2024-10-08 21:11:55 +0500

    Minor changes

--- src/components/ui/parallax-scroll.tsx
+++ src/components/ui/parallax-scroll.tsx
@@ -17,8 +17,8 @@ export const ParallaxScroll = ({
   const { scrollYProgress } = useScroll(); // Now tracking global scroll
 
   // Horizontal translations based on page scroll
-  const translateFirst = useTransform(scrollYProgress, [0, 1], [-500, 0]);
-  const translateSecond = useTransform(scrollYProgress, [0, 1], [500, 0]);
+  const translateFirst = useTransform(scrollYProgress, [0, 1], [-500, 400]);
+  const translateSecond = useTransform(scrollYProgress, [0, 1], [500, -400]);
 
   // Split the items into two groups (3 in each)
   const firstPart = items.slice(0, 3);
@@ -69,7 +69,7 @@ function ContentCard({
   return (
     <div className="max-w-xs w-full group/card">
       <Link href={link}>
-        <div className="cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto flex flex-col justify-between">
+        <div className="p-4 cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto flex flex-col justify-between">
           {/* Use Image component for optimized image loading */}
           <Image
             src={image}
@@ -79,15 +79,8 @@ function ContentCard({
             className="rounded-lg"
           />
           <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
-          <div className="flex flex-row items-center space-x-4 z-10">
-            <div className="flex flex-col">
-              <p className="font-normal text-base text-gray-50 relative z-10">
-                Manu Arora
-              </p>
-              <p className="text-sm text-gray-400">2 min read</p>
-            </div>
-          </div>
-          <div className="text content">
+          <div className="flex flex-row items-center space-x-4 z-10"></div>
+          <div className="text content ">
             <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
               {text}
             </h1>
