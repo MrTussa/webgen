b712010207384f77c6e31a03314bd7a74aa9eb40
MrTussa <dd993736@gmail.com>
2024-10-12 00:53:09 +0500

    Added: Projects Images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,17 +3,41 @@ import HeaderText from "./HeaderText";
 import { ParallaxScroll } from "./ui/parallax-scroll";
 
 const items = [
-  { image: "/border-bottom.svg", link: "/page1", text: "Card 1" },
-  { image: "/border-bottom.svg", link: "/page2", text: "Card 2" },
-  { image: "/border-bottom.svg", link: "/page3", text: "Card 3" },
-  { image: "/border-bottom.svg", link: "/page4", text: "Card 4" },
-  { image: "/border-bottom.svg", link: "/page5", text: "Card 5" },
-  { image: "/border-bottom.svg", link: "/page6", text: "Card 6" },
+  {
+    image: "/lama.png",
+    link: "https://next-ecommerce-dusky-eta.vercel.app/",
+    text: "Интернет Магазин с поддержкой админ панели и контролью склада.",
+  },
+  {
+    image: "/portfolio.jpg",
+    link: "https://mrtusa.uz/",
+    text: "Личное порфолио используя 3Д модели.",
+  },
+  {
+    image: "/tg-planner.png",
+    link: "https://github.com/sadlyOs/Planner-v1/tree/main",
+    text: "Мультиязычный бот планировщик с напоминаниями.",
+  },
+  {
+    image: "/tg-shop.png",
+    link: "https://github.com/sadlyOs/StoreBot",
+    text: "Интернет магазин в телеграме. Бот поддерживает авто-платежи",
+  },
+  {
+    image: "/mattress.jpg",
+    link: "/page5",
+    text: "Оформление карточек для маркетплейсов",
+  },
+  {
+    image: "/marusya.png",
+    link: "/page6",
+    text: "Фотореалистичная модель Marussia B2",
+  },
 ];
 
 const Projects = () => {
   return (
-    <section className="bg-black-secondary z-0 w-full px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 relative">
+    <section className="pb-4 bg-black-secondary z-0 w-full px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 relative overflow-hidden">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-dot-black -z-10 [mask-image:radial-gradient(ellipse_at_center,black_30%,transparent)]" />
       <HeaderText text="Наши Работы" />
       <ParallaxScroll items={items} />
