f963c0228f2a6ee030bf3ec9aea064669d47b33a
MrTussa <dd993736@gmail.com>
2024-10-12 00:52:30 +0500

    Added: image header

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -38,11 +38,13 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex space-y-4",
+        " row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex space-y-4",
         className
       )}
     >
-      {header}
+      <div className="relative flex flex-1 w-full h-full min-h-[3rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+        {header}
+      </div>
 
       <div
         className={`group-hover/bento:translate-x-2 transition duration-200 ${
